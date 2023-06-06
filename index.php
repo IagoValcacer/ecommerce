a12710364580f6f70c81e531e9b77574c7615bcb
IagoValcacer <iagoh210@gmail.com>
2023-06-06 17:09:58 -0300

    Tela de Login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+  
 $app = new Slim();
 
 $app->config('debug', true);
@@ -18,6 +20,9 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
+
 	//chama o construct
 	$page = new PageAdmin();
 
@@ -25,6 +30,33 @@ $app->get('/admin', function() {
 	$page->setTpl("index");
 });
 
+$app->get('/admin/login', function() {
+	//chama o construct
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	//chama o arquivo h1
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+	//chama o construct
+	User::login($_POST["login"], $_POST["password"]);
+
+	//chama o arquivo h1
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+	//chama o construct
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
 // 
 $app->run();
 
