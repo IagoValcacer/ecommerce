a74ecff4c5924a0223ee9b18a7fa3e63a1f2a044
IagoValcacer <iagoh210@gmail.com>
2023-06-06 15:44:31 -0300

    Criando page admin

--- index.php
+++ index.php
@@ -3,22 +3,28 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use Hcode\Page;
-
+use \Hcode\Page;
+use \Hcode\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
 //rota que chamamos
 $app->get('/', function() {
-
 	//chama o construct
 	$page = new Page();
 
 	//chama o arquivo h1
 	$page->setTpl("index");
-
-
 });
+
+$app->get('/admin', function() {
+	//chama o construct
+	$page = new PageAdmin();
+
+	//chama o arquivo h1
+	$page->setTpl("index");
+});
+
 // 
 $app->run();
 
